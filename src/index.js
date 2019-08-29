416b390aacb4011429999bd4640b6daa98d76640
Alexey Popov <alexey.popov@5thkind.com>
2019-08-29 14:43:15 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import _ from 'lodash';
+
+function component() {
+  const element = document.createElement('div');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
+
+const error = () => {
+  try {
+    throw 'fff'
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+error();
